06dd18361ded275400ad9e0a722fd30eee550b0c
TruemanV5 <52494514+TruemanV5@users.noreply.github.com>
2025-06-26 11:30:47 +0800

    Create generate_sub.sh

---
+++ Method/generate_sub.sh
@@ -0,0 +1,7 @@
+python generate_sub.py \
+    --queries path/test_queries.txt \
+    --checkpoint path/geotext_official_checkpoint.pth \
+    --config path/Method/config.yaml \
+    --images path/Track_4_Phase_I_Images \
+    --output submission.txt \
+    --batch_size 32 \
